5ededd1e98e8299986306dab5256f1579067e80d
jubaquelagun <john.ubaquelaguna727@comunidadunir.net>
2025-02-03 21:33:25 -0500

    se modifica archivo readme

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # ms-books-catalogue
-Ejemplo de aplicación de Inventario
+Microservicio para la consulta y gestion de libros
 
-El archivo `DWFS 8.1 - Products.postman_collection.json` contiene una colección de Postman con ejemplos de las peticiones que se pueden realizar. Dentro de la colección, las peticiones están en la carpeta `SQL`.
--- src/main/java/com/unir/books/service/BookServiceImp.java
+++ src/main/java/com/unir/books/service/BookServiceImp.java
@@ -9,7 +9,6 @@ import com.unir.books.controller.model.BookDto;
 import com.unir.books.controller.model.CreateBookRequest;
 import com.unir.books.data.BookRepository;
 import com.unir.books.data.model.Book;
-import com.unir.books.data.utils.BookMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
